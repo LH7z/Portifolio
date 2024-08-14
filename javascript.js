c6b7d654bf6263ec164ea2c168291c8ca8c6cb53
luis henrique <luisham2001@gmail.com>
2024-08-14 10:32:23 -0300

    work and home

---
+++ javascript.js
@@ -0,0 +1,41 @@
+const links = document.querySelectorAll("a")
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function(e) {
+
+  })
+}
+
+const nav = document.querySelector("nav")
+var oldScroll = window.scrollY
+window.onscroll = function() {
+  console.log(window.scrollY);
+  if (oldScroll > window.scrollY) {
+    nav.style.top = "0"
+  } else {
+    nav.style.top = "-80px"
+  }
+  oldScroll = window.scrollY
+}
+
+
+const the_animation = document.querySelectorAll('.animation')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+
+    })
+},
+   { threshold: 0.5
+   });
+//
+  for (let i = 0; i < the_animation.length; i++) {
+   const elements = the_animation[i];
+
+    observer.observe(elements);
+  }
